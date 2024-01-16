b9e46dfe26832cdb090caecff74b25f0a974dcf6
Tony Salomone <dadmobile@gmail.com>
2024-01-16 14:58:11 -0500

    Add call to MLX via popen and TODOs for remaining work

--- transformerlab/plugins/mlx_exporter/main.py
+++ transformerlab/plugins/mlx_exporter/main.py
@@ -4,7 +4,9 @@ import sqlite3
 import argparse
 import sys
 
-# Connect to the LLM Lab database (to update job status)
+from mlx_lm import convert
+
+# TODO: Connect to the LLM Lab database (to update job status)
 # llmlab_root_dir = os.getenv('LLM_LAB_ROOT_PATH')
 # db = sqlite3.connect(llmlab_root_dir + "/workspace/llmlab.sqlite3")
 
@@ -13,13 +15,32 @@ import sys
 parser = argparse.ArgumentParser(description='Convert a model to MLX format.')
 parser.add_argument('--model_name', default='gpt-j-6b', type=str, help='Name of model to export.')
 parser.add_argument('--model_architecture', default='hf-causal', type=str, help='Type of model to export.')
+parser.add_argument('--experiment_name', default='', type=str, help='Name of experiment.')
 parser.add_argument('--model_adaptor', default='', type=str, help='Name of model adaptor.')
 args, unknown = parser.parse_known_args()
 
 # TODO: Verify that the model uses a supported format
+# According to MLX docs (as of Jan 16/24) supported formats are:
+# Mistral, Llama, Phi-2
 model_architecture = args.model_architecture
 print("Model architecture: ", model_architecture)
 
-# TODO: Call the MLX convert function
-print("Exporting model", args.model_name, "and adaptor", args.model_adaptor, "to MLX")
+# TODO: move the outputted model somewhere that the app can use it
+output_path = "."
+
+# Directory to run conversion subprocess
+root_dir = os.environ.get("LLM_LAB_ROOT_PATH")
+plugin_dir = f"{root_dir}/workspace/experiments/{args.experiment_name}/plugins/mlx_exporter"
+
+# TODO: Call the MLX convert function instead of just printing
+print(
+    ["python", '-m',  'mlx_lm.convert', 
+        '--hf-path', args.model_name, '--mlx-path', output_path, '-q'],
+)
+print("cwd:",plugin_dir)
+#subprocess.Popen(
+#    ["python", '-m',  'mlx_lm.convert', 
+#        '--hf-path', args.model_name, '--mlx-path', output_path, '-q'],
+#    cwd=plugin_dir,
+#)
 
