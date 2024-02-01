0e78596bc7c7a0fb2affe62770ff4d9cb5fc4a15
Tony Salomone <dadmobile@gmail.com>
2024-02-01 11:41:51 -0500

    Remove reference to adaptor in MLX exporter for now.

--- transformerlab/plugins/mlx_exporter/main.py
+++ transformerlab/plugins/mlx_exporter/main.py
@@ -15,12 +15,10 @@ root_dir = os.environ.get("LLM_LAB_ROOT_PATH")
 db = sqlite3.connect(root_dir + "/workspace/llmlab.sqlite3")
 
 # Get all arguments provided to this script using argparse
-# NOTE: This takes an adaptor name but doesn't do anything with the adaptor at this time
 parser = argparse.ArgumentParser(description='Convert a model to MLX format.')
 parser.add_argument('--model_name', default='gpt-j-6b', type=str, help='Name of model to export.')
 parser.add_argument('--model_architecture', default='hf-causal', type=str, help='Type of model to export.')
 parser.add_argument('--experiment_name', default='', type=str, help='Name of experiment.')
-parser.add_argument('--model_adaptor', default='', type=str, help='Name of model adaptor.')
 parser.add_argument('--quant_bits', default='4', type=str, help='Bits per weight for quantization.')
 parser.add_argument('--job_id', type=str, help='Job to update in the database.')
 args, unknown = parser.parse_known_args()
