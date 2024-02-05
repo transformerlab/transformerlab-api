08af62f372855765cd5f77cf14da1fe79c4d918a
Tony Salomone <dadmobile@gmail.com>
2024-02-05 08:28:02 -0500

    Set minimum version of mlx-lm to 0.0.6

--- transformerlab/plugins/mlx_lora_trainer/setup.sh
+++ transformerlab/plugins/mlx_lora_trainer/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 pip install trl
-pip install mlx-lm
+pip install mlx-lm>=0.0.6
 git clone https://github.com/ml-explore/mlx-examples.git
 # requires:
 # mlx>=0.0.7
--- transformerlab/plugins/mlx_server/setup.sh
+++ transformerlab/plugins/mlx_server/setup.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-pip install mlx-lm
+pip install mlx-lm>=0.0.6
