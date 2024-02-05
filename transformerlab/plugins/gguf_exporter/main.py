0f54aaffc1983f1f737d3f080b57ad720a97a205
Tony Salomone <dadmobile@gmail.com>
2024-02-05 11:43:07 -0500

    Update GGUF exporter to clone llama.cpp and call run it directly

--- transformerlab/plugins/gguf_exporter/main.py
+++ transformerlab/plugins/gguf_exporter/main.py
@@ -1,25 +1,35 @@
 # This plugin exports a model to GGUF format so you can interact and train on a MBP with Apple Silicon
 import os
 import subprocess
+import sys
 import argparse
 
 # Get all arguments provided to this script using argparse
 parser = argparse.ArgumentParser(description='Convert a model to GGUF format.')
 parser.add_argument('--output_dir', type=str, help='Directory to save the model in.')
 parser.add_argument('--model_name', default='gpt-j-6b', type=str, help='Name of model to export.')
-parser.add_argument('--quant_bits', default='4', type=str, help='Bits per weight for quantization.')
 args, unknown = parser.parse_known_args()
 
-output_path = args.output_dir
+# input arguments
+input_model_id = args.model_name
+output_filename = f"{input_model_id.split('/')[-1]}.gguf"
+output_path = os.path.join(args.output_dir, output_filename) 
 
 # Directory to run conversion subprocess
 plugin_dir = os.path.realpath(os.path.dirname(__file__))
 
+subprocess_cmd = [
+    sys.executable,
+    os.path.join(plugin_dir, 'llama.cpp', 'convert.py'),
+    '--outfile', output_path,
+    '--outtype', 'q4_0',
+    input_model_id
+]
+
 # Call GGUF Convert function
-# TODO: This is a placeholder for now! Sleep 10 seconds to test app functionality
+# TODO: This default quantizes to 4-bit. Need to read that in as a parameter.
 export_process = subprocess.run(
-#    ["python", '-u', '-m',  _TODO_conversion_class, '--hf-path', args.model_name, '--output-path', output_path, '-q', '--q-bits', str(args.quant_bits)],
-    ["sleep", '5'],
+    subprocess_cmd,
     cwd=plugin_dir,
     capture_output=True
 )
--- transformerlab/plugins/gguf_exporter/setup.sh
+++ transformerlab/plugins/gguf_exporter/setup.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-pip install llama-cpp-python
+# Unfortunately, llama-cpp-python doesn't support conversion yet
+# pip install llama-cpp-python
+# So need to copy llama.cpp and run from there
+git clone https://github.com/ggerganov/llama.cpp/
