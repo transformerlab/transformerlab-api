d43486db4f1cb5e8bdb3c62ad363d713794b47c5
Tony Salomone <dadmobile@gmail.com>
2024-03-07 15:44:19 -0500

    Peg MLX to 0.0.14 in all plugins

--- transformerlab/plugins/mlx_lora_trainer/setup.sh
+++ transformerlab/plugins/mlx_lora_trainer/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 pip install trl
-pip install "mlx-lm>=0.0.6"
+pip install "mlx-lm==0.0.14"
 git clone https://github.com/ml-explore/mlx-examples.git
 pip install tensorboardX # for tensorboard
 # requires:
