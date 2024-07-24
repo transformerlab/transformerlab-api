1769b0a76afb43fc26ce37ad1b5a60f24b433d50
Tony Salomone <dadmobile@gmail.com>
2024-07-24 13:53:22 -0400

    Update MLX to v0.16.1 to support Llama 3.1

--- transformerlab/plugins/mlx_lora_trainer/setup.sh
+++ transformerlab/plugins/mlx_lora_trainer/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 pip install trl
-pip install mlx==0.14.0 --upgrade
-pip install "mlx-lm==0.14.0" --upgrade
+pip install mlx==0.16.0 --upgrade
+pip install "mlx-lm==0.16.1" --upgrade
 git clone https://github.com/ml-explore/mlx-examples.git
 pip install tensorboardX # for tensorboard
 # requires:
