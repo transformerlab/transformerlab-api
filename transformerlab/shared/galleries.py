7806cb95e3c1a331fdb01909b121e8df61b13a03
Tony Salomone <dadmobile@gmail.com>
2024-08-02 16:14:20 -0400

    Update gallery caching to use local copy first then try remote.

--- transformerlab/shared/galleries.py
+++ transformerlab/shared/galleries.py
@@ -1,4 +1,8 @@
-# Functions for managing galleries
+# Transformer Lab updates some of its galleries remotely
+# It then tries to download the latest version and store in a local cache
+# with a backup stored in the server code.
+# This is all managed in this file.
+
 import os
 import shutil
 import urllib.request
@@ -15,18 +19,26 @@ def update_gallery_cache():
     Called when Transformer Lab starts up.
     Initializes any cached gallery files and tries to update from remote.
     """
+
+    # This is the list of galleries to update remotely
     filelist = [
         "model-gallery.json"
     ]
 
     for filename in filelist:
-        # First, if not cached then initialize with the local copy.
-        localfile = os.path.join(dirs.TFL_SOURCE_CODE_DIR, dirs.GALLERIES_SOURCE_PATH, filename)
-        if not os.path.isfile(localfile):
-            print("ERROR: Unable to find local gallery file", localfile)
-        #shutil.copyfile(src, dst)
+        # First, if nothing is cached yet, then initialize with the local copy.
+        cached_gallery_file = os.path.join(dirs.GALLERIES_CACHE_DIR, filename)
+        if not os.path.isfile(cached_gallery_file):
+            print(f"Initializing {filename} from local source.")
 
-        # Then try to update from remote.
+            sourcefile = os.path.join(dirs.TFL_SOURCE_CODE_DIR, dirs.GALLERIES_SOURCE_PATH, filename)
+            if os.path.isfile(sourcefile):
+                shutil.copyfile(sourcefile, cached_gallery_file)
+            else:
+                print("ERROR: Unable to find local gallery file", sourcefile)
+
+
+        # Then, try to update from remote.
         update_cache_from_remote(filename)
 
 
@@ -34,7 +46,11 @@ def update_cache_from_remote(gallery_filename: str):
     """
     Fetches a gallery file from source and updates the cache
     """
-    remote_gallery = REMOTE_GALLERY_DIR_URL + gallery_filename
-    local_cache_filename = os.path.join(dirs.GALLERIES_CACHE_DIR, gallery_filename)
-    print (f"Caching remote gallery: {remote_gallery}")
-    urllib.request.urlretrieve(remote_gallery, local_cache_filename)
+    try:
+        remote_gallery = REMOTE_GALLERY_DIR_URL + gallery_filename
+        local_cache_filename = os.path.join(dirs.GALLERIES_CACHE_DIR, gallery_filename)
+        urllib.request.urlretrieve(remote_gallery, local_cache_filename)
+        print (f"Updated gallery from remote: {remote_gallery}")
+    except Exception as e:
+        print(f"Failed to update gallery from remote: {remote_gallery}")
+
