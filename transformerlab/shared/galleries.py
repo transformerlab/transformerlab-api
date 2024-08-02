bf7cbc60b404bb2e7a230b1e4be6594588f3ebd2
Tony Salomone <dadmobile@gmail.com>
2024-08-02 15:52:18 -0400

    First copy local gallery to cache in case server is offline.

--- transformerlab/shared/galleries.py
+++ transformerlab/shared/galleries.py
@@ -1,5 +1,6 @@
 # Functions for managing galleries
 import os
+import shutil
 import urllib.request
 
 from transformerlab.shared import dirs
@@ -7,6 +8,26 @@ from transformerlab.shared import dirs
 
 TLAB_REMOTE_URL = "https://raw.githubusercontent.com/transformerlab/transformerlab-api/main/"
 REMOTE_GALLERY_DIR_URL = f"{TLAB_REMOTE_URL}{dirs.GALLERIES_SOURCE_PATH}"
+
+
+def update_gallery_cache():
+    """
+    Called when Transformer Lab starts up.
+    Initializes any cached gallery files and tries to update from remote.
+    """
+    filelist = [
+        "model-gallery.json"
+    ]
+
+    for filename in filelist:
+        # First, if not cached then initialize with the local copy.
+        localfile = os.path.join(dirs.TFL_SOURCE_CODE_DIR, dirs.GALLERIES_SOURCE_PATH, filename)
+        if not os.path.isfile(localfile):
+            print("ERROR: Unable to find local gallery file", localfile)
+        #shutil.copyfile(src, dst)
+
+        # Then try to update from remote.
+        update_cache_from_remote(filename)
 
 
 def update_cache_from_remote(gallery_filename: str):
