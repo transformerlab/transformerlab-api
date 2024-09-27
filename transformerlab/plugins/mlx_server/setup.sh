529a89faab8974440070463d552449c57591770d
Tony Salomone <dadmobile@gmail.com>
2024-09-27 10:00:35 -0400

    Update versions of mlx in all plugins

--- transformerlab/plugins/mlx_server/setup.sh
+++ transformerlab/plugins/mlx_server/setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-pip install mlx==0.16.0 --upgrade
-pip install "mlx-lm==0.16.1" --upgrade
+pip install mlx==0.17.3 --upgrade
+pip install "mlx-lm==0.18.2" --upgrade
 pip install "mlx_embedding_models==0.0.9"
--- transformerlab/plugins/mlx_vlm_server/setup.sh
+++ transformerlab/plugins/mlx_vlm_server/setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-pip install mlx==0.15.2 --upgrade
+pip install mlx==0.17.3 --upgrade
 pip install -U mlx-vlm
 pip install "mlx>=0.8.0" numpy transformers torch huggingface_hub Pillow
