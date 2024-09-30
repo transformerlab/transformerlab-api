12e937e3f89c1010c3e36a4a6d8aa61970f5a664
Tony Salomone <dadmobile@gmail.com>
2024-09-30 17:09:56 -0400

    Add a really basic calculator tool

---
+++ transformerlab/tools/calculator/main.py
@@ -0,0 +1,30 @@
+"""
+Basic calculator functions
+
+Sample math functions for demo purposes.
+"""
+
+def add(x: float, y: float) -> float:
+    """
+    Get the sum of two numbers.
+
+    Args:
+        x: The first number of type float to sum.
+        y: The second number of type float to sum.
+    Returns:
+        The sum of x and y as a number of type float.
+    """
+    return float(x)+float(y)
+
+
+def multiply(x: float, y: float) -> float:
+    """
+    Get the product you get from multiplying two numbers together.
+
+    Args:
+        x: The first number of type float to multiply.
+        y: The second number of type float to multiply.
+    Returns:
+        The product of x times y as a number of type float.
+    """
+    return float(x)*float(y)
