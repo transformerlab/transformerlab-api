e67d0ef602101375e912b31a913ccf6266e1634a
Tony Salomone <dadmobile@gmail.com>
2024-10-07 15:54:22 -0400

    Name the subtract method correctly in calculator tool.

--- transformerlab/tools/calculator/main.py
+++ transformerlab/tools/calculator/main.py
@@ -17,7 +17,7 @@ def add(x: float, y: float) -> float:
     return float(x)+float(y)
 
 
-def add(x: float, y: float) -> float:
+def subtract(x: float, y: float) -> float:
     """
     Get the difference between two numbers.
 
