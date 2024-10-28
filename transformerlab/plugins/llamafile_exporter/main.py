24a89c085384ab707dcf1a4c2be593e76bdcca1d
Tony Salomone <dadmobile@gmail.com>
2024-10-28 15:10:27 -0400

    Properly read model path to get GGUF file from anywhere on server.

--- transformerlab/plugins/llamafile_exporter/main.py
+++ transformerlab/plugins/llamafile_exporter/main.py
@@ -9,24 +9,22 @@ from huggingface_hub import snapshot_download
 # Get all arguments provided to this script using argparse
 parser = argparse.ArgumentParser(description='Create a fully contained self-executing llamafile.')
 parser.add_argument('--model_name', default='gpt-j-6b', type=str, help='Name of model to create llamafile from.')
+parser.add_argument('--model_path', default='gpt-j-6b', type=str, help='Path to directory or file containing the model.')
 parser.add_argument('--output_dir', type=str, help='Directory to save the model in.')
 
 # TODO: The app generates an ID but it's not consistent with how llamafiles are normally named
 #parser.add_argument('--output_model_id', type=str, help='ID of outputted llamafile.')
 args, unknown = parser.parse_known_args()
 
-# Figure out where the actual input model file is
-# The model _should_ be available locally
-# And we download GGUF files to the transformerlab models dir
-# TODO: This is not working!
+# We need to pass the model ID in the .args file
+# But we need the actual model path to get the GGUF file
 input_model = args.model_name
-input_model_path = input_model
-print("Input model path:", input_model_path)
+input_model_path = args.model_path
 
 # Directory to run conversion subprocess
 plugin_dir = os.path.realpath(os.path.dirname(__file__))
 
-# Output details
+# Output details - ignoring the output_model_id passed by app
 outfile = f"{input_model}.llamafile"
 output_dir = args.output_dir
 
