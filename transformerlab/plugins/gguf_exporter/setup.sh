842b02cfbcce3e92e09882bb7f05b57fb47174ce
Tony Salomone <dadmobile@gmail.com>
2024-10-29 14:41:32 -0400

    GGUF Exporter: Update setup to use known version. Add supported models.

--- transformerlab/plugins/gguf_exporter/setup.sh
+++ transformerlab/plugins/gguf_exporter/setup.sh
@@ -3,3 +3,5 @@
 # pip install llama-cpp-python
 # So need to copy llama.cpp and run from there
 git clone https://github.com/ggerganov/llama.cpp/
+cd llama.cpp
+git checkout 8f275a7c4593aa34147595a90282cf950a853690 # this is a known good version
