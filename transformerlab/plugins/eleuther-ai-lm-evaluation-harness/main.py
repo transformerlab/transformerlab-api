8c54329ff6a678b1b40e2d11bd4f8b1abb05c0ea
deep1401 <gandhi0869@gmail.com>
2025-01-28 15:34:17 -0800

    Remove dependency from local completions

--- transformerlab/plugins/eleuther-ai-lm-evaluation-harness/main.py
+++ transformerlab/plugins/eleuther-ai-lm-evaluation-harness/main.py
@@ -15,6 +15,7 @@ parser.add_argument('--model_type', default='hf-causal',
 parser.add_argument('--experiment_name', default='', type=str)
 parser.add_argument('--eval_name', default='', type=str)
 parser.add_argument('--task', default='', type=str)
+parser.add_argument('--model_adapter', default=None, type=str)
 
 
 args, other = parser.parse_known_args()
@@ -41,20 +42,25 @@ task = args.task
 if not torch.cuda.is_available():
     print("CUDA is not available. Running eval using HTTP.")
 
-    # first check http://localhost:8338/healthz
-    response = requests.get('http://localhost:8338/server/worker_healthz')
-    print(response.json())
-    if response.status_code != 200 or not isinstance(response.json(), list) or len(response.json()) == 0:
-        print("Local completions server is not running. Please start it before running the evaluation.")
-        sys.exit(0)
+    # # first check http://localhost:8338/healthz
+    # response = requests.get('http://localhost:8338/server/worker_healthz')
+    # print(response.json())
+    # if response.status_code != 200 or not isinstance(response.json(), list) or len(response.json()) == 0:
+    #     print("Local completions server is not running. Please start it before running the evaluation.")
+    #     sys.exit(0)
 
     # model name is the first item in the list:
     model_name = args.model_name
 
     # lm_eval --model local-completions --tasks gsm8k --model_args model=mlx-community/Llama-3.2-1B-Instruct-4bit,base_url=http://localhost:8338/v1/completions,num_concurrent=1,max_retries=3,tokenized_requests=False
     model_args = 'model=' + model_name + \
-        ',base_url=http://localhost:8338/v1/completions,num_concurrent=1,max_retries=3,tokenized_requests=False,trust_remote_code=True'
-    command = ["lm-eval", '--model', 'local-completions',
+        ',trust_remote_code=True'
+    if args.model_adapter:
+        adapter_path = os.path.join(
+            os.environ["_TFL_WORKSPACE_DIR"], 'adaptors', args.model_name, args.model_adapter)
+        model_args += f",peft={adapter_path}"
+
+    command = ["lm-eval", '--model', 'hf',
                '--model_args', model_args, '--tasks', task]
     print('Running command: $ ' + ' '.join(command))
     print("--Beginning to run evaluations (please wait)...")
@@ -71,6 +77,12 @@ if not torch.cuda.is_available():
         print(f"An error occurred while running the subprocess: {e}")
     print('--Evaluation task complete')
 else:
+
+    if args.model_adapter:
+        adapter_path = os.path.join(
+            os.environ["_TFL_WORKSPACE_DIR"], 'adaptors', args.model_name, args.model_adapter)
+        model_args += f",peft={adapter_path}"
+
     command = ["lm-eval",
                '--model_args', model_args, '--tasks', task, '--device', 'cuda:0', '--trust_remote_code']
 
