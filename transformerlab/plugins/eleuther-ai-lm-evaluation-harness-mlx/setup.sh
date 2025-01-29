6ff0f4e1146b53050fb4a25f207319f83c0a22e5
deep1401 <gandhi0869@gmail.com>
2025-01-29 10:29:14 -0800

    Changing version number, install logic

--- transformerlab/plugins/eleuther-ai-lm-evaluation-harness-mlx/setup.sh
+++ transformerlab/plugins/eleuther-ai-lm-evaluation-harness-mlx/setup.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+
+# Check if the 'lm-evaluation-harness-mlx' directory exists and remove it if it does
+if [ -d "./lm-evaluation-harness-mlx" ]; then
+  rm -rf ./lm-evaluation-harness-mlx
+fi
+
 # Clone the repository
 git clone https://github.com/chimezie/lm-evaluation-harness-mlx || { echo "Git clone failed or repository already exists"; exit 1; }
 
