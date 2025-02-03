13843e1dd1418c9f3e12a9a586b68c900ece2ecd
deep1401 <gandhi0869@gmail.com>
2025-02-03 11:51:07 -0800

    Adding default adapter path for lora trainer and pinning openai version for rag

--- transformerlab/plugins/llamaindex_simple_document_search/setup.sh
+++ transformerlab/plugins/llamaindex_simple_document_search/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 pip install llama-index
 pip install llama-index-llms-openai-like
+pip install openai==1.61.0
 pip install llama-index-embeddings-huggingface
 pip install cryptography # needed to read PDFs
