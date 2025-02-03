eaeafcfd5aa30bb78cfc91222c03e550b8f03d34
sanjaycal <sanjay.calgary@gmail.com>
2025-02-03 12:42:44 -0500

    update llama_index plugin to use uv

--- transformerlab/plugins/llamaindex_simple_document_search/setup.sh
+++ transformerlab/plugins/llamaindex_simple_document_search/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pip install llama-index
-pip install llama-index-llms-openai-like
-pip install llama-index-embeddings-huggingface
-pip install cryptography # needed to read PDFs
+uv pip install llama-index
+uv pip install llama-index-llms-openai-like
+uv pip install llama-index-embeddings-huggingface
+uv pip install cryptography # needed to read PDFs
