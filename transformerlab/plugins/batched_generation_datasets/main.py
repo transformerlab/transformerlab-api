fd216b7e4ef6fa6210c5754676e6a2bdbc6ffd14
deep1401 <gandhi0869@gmail.com>
2025-02-18 14:49:24 -0800

    Add batching requests for any kind of model

--- transformerlab/plugins/batched_generation_datasets/main.py
+++ transformerlab/plugins/batched_generation_datasets/main.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import sys
 from typing import List
+import asyncio
 
 import instructor
 import pandas as pd
@@ -15,16 +16,16 @@ from pydantic import BaseModel
 from tqdm import tqdm
 from langchain.schema import SystemMessage, HumanMessage
 
+from requests_batching import process_dataset
 
-parser = argparse.ArgumentParser(
-    description="Run Synthesizer for generating data.")
+
+parser = argparse.ArgumentParser(description="Run Synthesizer for generating data.")
 parser.add_argument(
     "--run_name",
     default="test",
     type=str,
 )
-parser.add_argument("--model_name", default="gpt-j-6b",
-                    type=str, help="Model to use for evaluation.")
+parser.add_argument("--model_name", default="gpt-j-6b", type=str, help="Model to use for evaluation.")
 parser.add_argument("--dataset_name", default="test", type=str)
 parser.add_argument(
     "--model_adapter",
@@ -38,6 +39,7 @@ parser.add_argument("--output_column", default=None, type=str)
 parser.add_argument("--experiment_name", default="test", type=str)
 parser.add_argument("--job_id", default=None, type=str)
 parser.add_argument("--system_prompt", default=None, type=str)
+parser.add_argument("--batch_size", default=128, type=int)
 
 
 args, other = parser.parse_known_args()
@@ -54,7 +56,6 @@ job.update_progress(0)
 
 def check_local_server():
     response = requests.get("http://localhost:8338/server/worker_healthz")
-    print(response.json())
     if response.status_code != 200 or not isinstance(response.json(), list) or len(response.json()) == 0:
         print("Local Model Server is not running. Please start it before running the evaluation.")
         sys.exit(1)
@@ -66,26 +67,22 @@ def fetch_dataset():
         sys.exit(1)
     try:
         params = {"dataset_id": args.dataset_name, "split": args.dataset_split}
-        response = requests.get(
-            "http://localhost:8338/data/get", params=params)
+        response = requests.get("http://localhost:8338/data/get", params=params)
         if response.status_code != 200:
             print(f"Error fetching the dataset: {response.json()}")
-            job.set_job_completion_status(
-                "failed", f"Error fetching the dataset: {response.json()}")
+            job.set_job_completion_status("failed", f"Error fetching the dataset: {response.json()}")
             sys.exit(1)
         else:
             df = pd.DataFrame(response.json()["data"]["columns"])
             file_location = response.json()["data"]["file_location"]
             if file_location is None or not os.path.exists(file_location):
                 file_location = os.path.join(
-                    os.environ.get(
-                        "_TFL_WORKSPACE_DIR"), "datasets", args.dataset_name + "_with_outputs"
+                    os.environ.get("_TFL_WORKSPACE_DIR"), "datasets", args.dataset_name + "_with_outputs"
                 )
             return df, file_location
     except Exception as e:
         print(f"An error occurred while fetching the dataset: {e}")
-        job.set_job_completion_status(
-            "failed", f"An error occurred while fetching the dataset: {e}")
+        job.set_job_completion_status("failed", f"An error occurred while fetching the dataset: {e}")
         sys.exit(1)
 
 
@@ -108,14 +105,26 @@ class TRLAB_MODEL(DeepEvalBaseLLM):
             if message["role"] == "system":
                 modified_messages.append(SystemMessage(**message))
             else:
-                modified_messages.append(HumanMessage(**message
-                                                      ))
+                modified_messages.append(HumanMessage(**message))
         return chat_model.invoke(modified_messages).content
 
     async def a_generate(self, prompt: str) -> str:
         chat_model = self.load_model()
         res = await chat_model.ainvoke(prompt)
         return res.content
+
+    async def generate_batched(self, df: pd.DataFrame, sys_prompt_col=None) -> pd.DataFrame:
+        updated_df = await process_dataset(
+            df,
+            batch_size=args.batch_size,
+            model=args.model_name,
+            inference_url="http://localhost:8338/v1/chat/completions",
+            api_key="dummy",
+            sys_prompt_col=sys_prompt_col,
+            input_col=args.input_column,
+            output_col=args.output_column,
+        )
+        return updated_df
 
     def get_model_name(self):
         return args.model_name
@@ -128,16 +137,14 @@ class CustomCommercialModel(DeepEvalBaseLLM):
         if model_type == "claude":
             if os.environ.get("ANTHROPIC_API_KEY") is None:
                 print("Please set the Anthropic API Key from Settings.")
-                job.set_job_completion_status(
-                    "failed", "Please set the Anthropic API Key from Settings.")
+                job.set_job_completion_status("failed", "Please set the Anthropic API Key from Settings.")
                 sys.exit(1)
             self.model = Anthropic()
 
         elif model_type == "openai":
             if os.environ.get("OPENAI_API_KEY") is None:
                 print("Please set the OpenAI API Key from Settings.")
-                job.set_job_completion_status(
-                    "failed", "Please set the OpenAI API Key from Settings.")
+                job.set_job_completion_status("failed", "Please set the OpenAI API Key from Settings.")
                 sys.exit(1)
             self.model = OpenAI()
 
@@ -184,6 +191,27 @@ class CustomCommercialModel(DeepEvalBaseLLM):
     async def a_generate(self, prompt: str, schema: BaseModel) -> BaseModel:
         return self.generate(prompt, schema)
 
+    async def generate_batched(self, df: pd.DataFrame, sys_prompt_col=None) -> pd.DataFrame:
+        INFERENCE_URL = (
+            "https://api.openai.com/v1/chat/completions"
+            if self.model_type == "openai"
+            else "https://api.anthropic.com/v1/chat/completions"
+        )
+        api_key = (
+            os.environ.get("OPENAI_API_KEY") if self.model_type == "openai" else os.environ.get("ANTHROPIC_API_KEY")
+        )
+        updated_df = await process_dataset(
+            df,
+            batch_size=args.batch_size,
+            model=self.model,
+            inference_url=INFERENCE_URL,
+            api_key=api_key,
+            sys_prompt_col=sys_prompt_col,
+            input_col=args.input_column,
+            output_col=args.output_column,
+        )
+        return updated_df
+
     def get_model_name(self):
         return self.model_name
 
@@ -191,11 +219,9 @@ class CustomCommercialModel(DeepEvalBaseLLM):
 try:
     if "local" not in args.generation_model.lower():
         if "openai" in args.generation_model.lower():
-            trlab_model = CustomCommercialModel(
-                "openai", args.generation_model)
+            trlab_model = CustomCommercialModel("openai", args.generation_model)
         else:
-            trlab_model = CustomCommercialModel(
-                "claude", args.generation_model)
+            trlab_model = CustomCommercialModel("claude", args.generation_model)
     else:
         check_local_server()
         custom_model = ChatOpenAI(
@@ -207,35 +233,36 @@ try:
 
 except Exception as e:
     print(f"An error occurred while loading the model: {e}")
-    job.set_job_completion_status(
-        "failed", "An error occurred while loading the model")
+    job.set_job_completion_status("failed", "An error occurred while loading the model")
     sys.exit(1)
 
 print("Model loaded successfully")
 job.update_progress(0.5)
 
 
-def run_batched_generation():
+async def run_batched_generation():
     try:
         check_local_server()
         df, file_location = fetch_dataset()
-        responses = []
-        for index, row in tqdm(df.iterrows()):
-            messages = []
-            if args.system_prompt:
-                messages.append(
-                    {"role": "system", "content": args.system_prompt})
-            messages.append(
-                {"role": "user", "content": row[args.input_column]})
-            response = trlab_model.generate_without_instructor(
-                messages=messages)
-            print("RESPONSE", response)
-            responses.append(response)
+        # responses = []
+        # for index, row in tqdm(df.iterrows()):
+        #     messages = []
+        #     if args.system_prompt:
+        #         messages.append({"role": "system", "content": args.system_prompt})
+        #     messages.append({"role": "user", "content": row[args.input_column]})
+        #     response = trlab_model.generate_without_instructor(messages=messages)
+        #     responses.append(response)
 
-        df[args.output_column] = responses
+        # df[args.output_column] = responses
+        sys_prompt_col = None
+        if args.system_prompt:
+            df["system_prompt"] = args.system_prompt
+            sys_prompt_col = "system_prompt"
+        df = await trlab_model.generate_batched(df, sys_prompt_col=sys_prompt_col)
+        print("Batched generation completed successfully")
+        job.update_progress(99)
         # List all files in the directory file_location ending with .json
-        json_files = [
-            f for f in os.listdir(file_location) if f.endswith(".json")]
+        json_files = [f for f in os.listdir(file_location) if f.endswith(".json")]
         # Iterate over the list of json files
         for json_file in json_files:
             # Get the full path of the file
@@ -243,19 +270,21 @@ def run_batched_generation():
             # Delete the file
             os.remove(file_path)
         print("Replacing the old dataset with the new one having outputs...")
-        file_name = os.path.join(
-            file_location, args.dataset_name + "_" + args.job_id + "_with_outputs.json")
+        file_name = os.path.join(file_location, args.dataset_name + "_" + args.job_id + "_with_outputs.json")
 
         df.to_json(file_name, orient="records", lines=False)
+        job.update_progress(100)
+        job.set_job_completion_status(
+            "success", f"Dataset {args.dataset_name} updated successfully", additional_output_path=file_name
+        )
 
         print("Updated the dataset successfully")
     except Exception as e:
         print(f"An error occurred while running batched generation: {e}")
-        job.set_job_completion_status(
-            "failed", f"An error occurred while running batched generation: {e}")
+        job.set_job_completion_status("failed", f"An error occurred while running batched generation: {e}")
         sys.exit(1)
 
 
 print("Running batched generation...")
-run_batched_generation()
+asyncio.run(run_batched_generation())
 # run_generation()
