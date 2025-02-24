397ed9790e82e02c062be0c074844107fd8bd287
deep1401 <gandhi0869@gmail.com>
2025-02-24 13:43:25 -0800

    Remove pip install wandb from plugins as it is global now

--- transformerlab/plugins/autotrain_sft_trainer/setup.sh
+++ transformerlab/plugins/autotrain_sft_trainer/setup.sh
@@ -1,3 +1,2 @@
 uv pip install autotrain-advanced
 uv pip install tensorboardX # for tensorboard logging
-uv pip install wandb
--- transformerlab/plugins/mlx_lora_trainer/setup.sh
+++ transformerlab/plugins/mlx_lora_trainer/setup.sh
@@ -3,7 +3,6 @@ uv pip install trl
 uv pip install mlx==0.22.0 --upgrade
 uv pip install "mlx-lm==0.21.1" --upgrade
 uv pip install tensorboardX # for tensorboard
-uv pip install wandb
 # requires:
 # mlx>=0.0.7
 # transformers
