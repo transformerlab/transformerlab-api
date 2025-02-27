5bb37803e7b442bd7cdb32c61062099fca4b1338
sanjaycal <sanjay.calgary@gmail.com>
2025-02-27 16:19:00 -0500

    fix api

--- transformerlab/routers/workflows.py
+++ transformerlab/routers/workflows.py
@@ -198,9 +198,9 @@ async def start_next_step_in_workflow():
     next_job_type =next_node["type"]
     next_job_status = "QUEUED" 
 
-    if next_job_type == "DOWNLOAD_MODEL":
+    #if next_job_type == "DOWNLOAD_MODEL":
 
-    elif "template" in next_node.keys():
+    if "template" in next_node.keys():
         template_name = next_node["template"] 
         if next_job_type == "TRAIN":
             next_job_data = await db.get_training_template_by_name(template_name)
