f3eb3cb51f7ab7b27ee3ccaec71e15d11cf54027
deep1401 <gandhi0869@gmail.com>
2025-03-11 09:34:05 -0700

    Fix wandb import issues due to protobuf upgrades from mlx-lm and remove --upgrade flag

--- transformerlab/plugins/mlx_lora_trainer/setup.sh
+++ transformerlab/plugins/mlx_lora_trainer/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 uv pip install trl
-uv pip install mlx==0.22.0 --upgrade
-uv pip install "mlx-lm==0.21.1" --upgrade
+uv pip install mlx==0.22.0
+uv pip install "mlx-lm==0.21.1"
 uv pip install tensorboardX # for tensorboard
 # requires:
 # mlx>=0.0.7
