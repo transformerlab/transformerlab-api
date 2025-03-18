d080bcf2587b61f70e43a1f867d1e72dcf653f7c
deep1401 <gandhi0869@gmail.com>
2025-03-18 11:04:30 -0700

    Changes

--- transformerlab/plugins/fastchat_server/setup.sh
+++ transformerlab/plugins/fastchat_server/setup.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
 # Everything should be installed by default
+# git clone https://github.com/transformerlab/FastChat
+# cd FastChat
+# uv pip install -e .
+
+# uv pip install "git+https://github.com/transformerlab/FastChat"
