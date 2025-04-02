d531ae42e96e271fdfc0e4e4f88e9307b43c31b5
deep1401 <gandhi0869@gmail.com>
2025-04-02 12:50:16 -0400

    Combined bug fixes for RAG, SFTTrainer depreciation, Pretrainer model json

--- transformerlab/plugins/llamaindex_simple_document_search/setup.sh
+++ transformerlab/plugins/llamaindex_simple_document_search/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 uv pip install llama-index
 uv pip install llama-index-llms-openai-like
-uv pip install openai==1.61.0
+uv pip install openai==1.70.0
 uv pip install llama-index-embeddings-huggingface
 uv pip install cryptography # needed to read PDFs
 # uv pip install xformers
