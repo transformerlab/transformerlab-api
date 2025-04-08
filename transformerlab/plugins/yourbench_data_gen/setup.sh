fcc94a013b64f3123d5a51599dce7345c7bb0ec6
deep1401 <gandhi0869@gmail.com>
2025-04-08 15:09:24 -0400

    Remove uv venv stuff

--- transformerlab/plugins/yourbench_data_gen/setup.sh
+++ transformerlab/plugins/yourbench_data_gen/setup.sh
@@ -4,9 +4,9 @@ cd yourbench
 
 # Use uv to install the dependencies
 # pip install uv # if you do not have uv already
-uv venv
-source .venv/bin/activate
-uv sync
+# uv venv
+# source .venv/bin/activate
+# uv sync
 uv pip install -e .
 # Need this to keep the current versions and preserve my sanity
 uv pip install torch==2.6.0 --upgrade
