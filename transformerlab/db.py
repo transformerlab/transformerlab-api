fbe4e301545074ab98a438aaf3b0cd457763352e
deep1401 <gandhi0869@gmail.com>
2025-04-14 12:57:47 -0400

    Remove PRAGMA from aio connection

--- transformerlab/db.py
+++ transformerlab/db.py
@@ -35,9 +35,7 @@ async def init():
     global db
     os.makedirs(os.path.dirname(DATABASE_FILE_NAME), exist_ok=True)
     db = await aiosqlite.connect(DATABASE_FILE_NAME)
-    db.execute("PRAGMA journal_mode=WAL")
-    db.execute("PRAGMA synchronous=normal")
-    db.execute("PRAGMA busy_timeout = 5000")
+
     global db_sync
     db_sync = sqlite3.connect(DATABASE_FILE_NAME, isolation_level=None)
     db_sync.execute("PRAGMA journal_mode=WAL")
