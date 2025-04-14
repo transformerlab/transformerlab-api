6c9d60db48d45fdcaeddc6324097a6a0c0ede42d
deep1401 <gandhi0869@gmail.com>
2025-04-14 11:36:37 -0400

    Fix YourBench Plugin bugs

--- transformerlab/plugins/yourbench_data_gen/setup.sh
+++ transformerlab/plugins/yourbench_data_gen/setup.sh
@@ -1,13 +1,13 @@
 # 1. Clone the repo
 git clone https://github.com/huggingface/yourbench.git
 cd yourbench
+git switch -c eb50e1fff84849e19ecb62b45a25ed6afed46a2e
 
-# Use uv to install the dependencies
-# pip install uv # if you do not have uv already
-# uv venv
-# source .venv/bin/activate
-# uv sync
+
+uv venv
+source .venv/bin/activate
+uv sync
 uv pip install -e .
-# Need this to keep the current versions and preserve my sanity
+# Need this to keep the current versions and preserve sanity
 uv pip install torch==2.6.0 --upgrade
 uv pip install textstat evaluate
