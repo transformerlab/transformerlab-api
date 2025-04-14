50426b78ae1933f3e74f38d6ab24490b02cd0f53
Tony Salomone <dadmobile@gmail.com>
2025-04-14 11:14:05 -0400

    db execute calls should be awaited.

--- transformerlab/db.py
+++ transformerlab/db.py
@@ -34,9 +34,9 @@ async def init():
     global db
     os.makedirs(os.path.dirname(DATABASE_FILE_NAME), exist_ok=True)
     db = await aiosqlite.connect(DATABASE_FILE_NAME)
-    db.execute("PRAGMA journal_mode=WAL")
-    db.execute("PRAGMA synchronous=normal")
-    db.execute("PRAGMA busy_timeout=5000")
+    await db.execute("PRAGMA journal_mode=WAL")
+    await db.execute("PRAGMA synchronous=normal")
+    await db.execute("PRAGMA busy_timeout=5000")
 
     # Create the tables if they don't exist
     async with async_engine.begin() as conn:
