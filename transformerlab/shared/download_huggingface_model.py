60d932acfea7d2fa178eda21f1fb372e0b86d4c5
Tony Salomone <dadmobile@gmail.com>
2025-04-24 09:31:36 -0400

    One more place where we call resume_download

--- transformerlab/shared/download_huggingface_model.py
+++ transformerlab/shared/download_huggingface_model.py
@@ -169,7 +169,7 @@ def download_blocking(model_is_downloaded):
             f.write(json.dumps(info, indent=2))
     else:
         try:
-            snapshot_download(repo_id=model, resume_download=True, allow_patterns=allow_patterns)
+            snapshot_download(repo_id=model, allow_patterns=allow_patterns)
 
         except GatedRepoError:
             returncode = 77
