b2a64c6a4d9be633b3060455880f9474c976e429
ali asaria <aliasaria@users.noreply.github.com>
2025-04-29 11:43:28 -0400

    Add Qwen3 Support to GGUF Exporter.
    Upgrading GGUF to the latest release.

--- transformerlab/plugins/gguf_exporter/setup.sh
+++ transformerlab/plugins/gguf_exporter/setup.sh
@@ -2,6 +2,10 @@
 # Unfortunately, llama-cpp-python doesn't support conversion yet
 # pip install llama-cpp-python
 # So need to copy llama.cpp and run from there
+#!/usr/bin/env bash
+# Unfortunately, llama-cpp-python doesn't support conversion yet
+# pip install llama-cpp-python
+# So need to copy llama.cpp and run from there
 git clone https://github.com/ggerganov/llama.cpp/
 cd llama.cpp
-git checkout 8f275a7c4593aa34147595a90282cf950a853690 # this is a known good version
+git checkout tags/b5219 # check out the tag called "b5219" which is the latest release april 29 2025
