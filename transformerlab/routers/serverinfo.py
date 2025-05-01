ff032405ce72a5a9530bd59c7eef0a92b088232c
Deep Gandhi <gandhi0869@gmail.com>
2025-05-01 13:01:06 -0400

    Remove redundant print statement

--- transformerlab/routers/serverinfo.py
+++ transformerlab/routers/serverinfo.py
@@ -221,7 +221,6 @@ async def get_computer_information():
             # info["temp"] = nvmlDeviceGetTemperature(handle)
             g.append(info)
     except Exception as e:  # Catch all exceptions and print them
-        print(f"Error retrieving GPU information: {e}")
 
         g.append(
             {
