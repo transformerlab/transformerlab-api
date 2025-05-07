687115ce231fdfa12b72f410eeae9b5868d23a2a
ali asaria <aliasaria@users.noreply.github.com>
2025-05-07 11:17:48 -0400

    try a fixed port

--- test/api/conftest.py
+++ test/api/conftest.py
@@ -6,15 +6,16 @@ import pytest
 @pytest.fixture(scope="session")
 def live_server():
     # Get a free port
-    import socket
+    # import socket
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(("127.0.0.1", 0))
-    port = s.getsockname()[1]
-    s.close()
+    # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # s.bind(("127.0.0.1", 0))
+    # port = s.getsockname()[1]
+    # s.close()
 
-    # Start the server process
-    print("about to run: ./run.sh -p", port)
+    # # Start the server process
+    # print("about to run: ./run.sh -p", port)
+    port = 8000  # For testing, we can use a fixed port
     server_process = subprocess.Popen(["./run.sh", "-h", "127.0.0.1", "-p", str(port)])
 
     # Give it time to start
