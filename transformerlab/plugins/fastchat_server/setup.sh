af0cf88a25a3d55eb7203202fa311db47d0ca15b
deep1401 <gandhi0869@gmail.com>
2025-05-13 15:53:00 -0400

    Increase version number of transformerlab-inference

--- transformerlab/plugins/fastchat_server/setup.sh
+++ transformerlab/plugins/fastchat_server/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Everything should be installed by default
-uv pip install "transformerlab-inference>=0.2.38"
+uv pip install "transformerlab-inference>=0.2.39"
 uv pip install bitsandbytes
 
 # Uncomment and replace with your local installation for testing
