80a994cd71334880756eb1652f41f66557166779
deep1401 <gandhi0869@gmail.com>
2025-05-27 11:11:38 -0400

    Delete max samples param from yml to train on larger datasets

--- transformerlab/plugins/sft_llama_factory/main.py
+++ transformerlab/plugins/sft_llama_factory/main.py
@@ -121,6 +121,10 @@ def run_train():
     print("Template configuration:")
     print(yml)
 
+    # Remove max_samples if it exists to use all training data
+    if "max_samples" in yml:
+        del yml["max_samples"]
+
     yml["model_name_or_path"] = tlab_trainer.params.model_name
     yml["output_dir"] = tlab_trainer.params.adaptor_output_dir
     yml["logging_dir"] = output_dir
