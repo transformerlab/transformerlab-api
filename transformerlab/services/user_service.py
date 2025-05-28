0efa68dc888d23d1a8dd5eeef8dcfe1a8bb6438c
Tony Salomone <dadmobile@gmail.com>
2025-05-28 14:47:39 -0400

    Update import to reflect moving User schema.

--- transformerlab/services/user_service.py
+++ transformerlab/services/user_service.py
@@ -11,7 +11,8 @@ from fastapi_users.authentication import (
 
 from fastapi_users.db import SQLAlchemyUserDatabase
 
-from db import User, get_user_db
+from transformerlab.db import get_user_db
+from transformerlab.shared.models.models import User
 
 SECRET = "TEMPSECRET"
 
