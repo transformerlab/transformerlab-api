dd11b50b9da9904803c91fefa2db8d4a461e27dd
Aahaan Maini <aahaanmaini@gmail.com>
2025-05-28 16:55:23 -0400

    Removed non-feature changes

--- transformerlab/plugins/mlx_exporter/main.py
+++ transformerlab/plugins/mlx_exporter/main.py
@@ -17,9 +17,6 @@ def mlx_export():
     plugin_dir = os.path.realpath(os.path.dirname(__file__))
     python_executable = get_python_executable(plugin_dir)
 
-    env = os.environ.copy()
-    env["PATH"] = python_executable.replace("/python", ":") + env["PATH"]
-
     command = [
         python_executable, "-u", "-m", "mlx_lm", "convert",
         "--hf-path", tlab_exporter.params.get("model_name"),
@@ -40,7 +37,6 @@ def mlx_export():
             cwd=plugin_dir,
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT,
-            env=env,
             universal_newlines=True,
             bufsize=1
         ) as process:
