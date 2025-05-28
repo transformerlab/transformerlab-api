814e7fbe9339a670914286f5ca38987d653957c9
Tony Salomone <dadmobile@gmail.com>
2025-05-28 13:30:09 -0400

    Add FastAPI schemas that are used to create endpoints.

---
+++ transformerlab/schemas/user.py
@@ -0,0 +1,19 @@
+"""
+User schemas for FastAPI Users integration.
+"""
+
+import uuid
+
+from fastapi_users import schemas
+
+
+class UserRead(schemas.BaseUser[uuid.UUID]):
+    pass
+
+
+class UserCreate(schemas.BaseUserCreate):
+    pass
+
+
+class UserUpdate(schemas.BaseUserUpdate):
+    pass
