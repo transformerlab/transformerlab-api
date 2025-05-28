81f9ecd35f9cf0089573bc16417009c4ee872f64
Mina Parham <minaparham@Tonys-MacBook-Pro.local>
2025-05-28 16:17:10 -0400

    add logger.info to trace model_name issue

--- transformerlab/plugins/openai_server/main.py
+++ transformerlab/plugins/openai_server/main.py
@@ -348,8 +348,8 @@ def main():
     args, _ = parser.parse_known_args()
 
     parameters = json.loads(args.parameters)
-    print(args.parameters)
-    print(parameters)
+    logger.info(args.parameters)
+    logger.info(parameters)
     model_name = str(parameters.get("model_name", "gpt-3.5-turbo"))
     
 
