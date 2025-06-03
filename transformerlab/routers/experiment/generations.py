d43848ec67cc2b751ddfd6f30db03d486314f295
Aahaan Maini <aahaanmaini@gmail.com>
2025-06-03 12:29:45 -0400

    bug fix

--- transformerlab/routers/experiment/generations.py
+++ transformerlab/routers/experiment/generations.py
@@ -221,11 +221,7 @@ async def run_generation_script(experimentId: int, plugin_name: str, generation_
     extra_args = ["--plugin_dir", script_directory]
     for key in template_config:
         extra_args.append("--" + key)
-        value = template_config[key]
-        if isinstance(value, (list, dict)):
-            extra_args.append(json.dumps(value))
-        else:
-            extra_args.append(value)
+        extra_args.append(template_config[key])
 
     extra_args.extend(
         [
