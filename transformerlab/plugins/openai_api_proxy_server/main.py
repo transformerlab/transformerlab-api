71c3972600e84599af395ebf7897f98d948c919b
Mina Parham <minaparham@Tonys-MacBook-Pro.local>
2025-06-09 17:35:30 -0400

    Add openai proxy server main.py

--- transformerlab/plugins/openai_api_proxy_server/main.py
+++ transformerlab/plugins/openai_api_proxy_server/main.py
@@ -12,59 +12,19 @@ except ImportError:
 
 # Get all arguments provided to this script using argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("--model-path", type=str)
+# parser.add_argument("--model-path", type=str)
 parser.add_argument("--parameters", type=str, default="{}")
 args, unknown = parser.parse_known_args()
 
-model = args.model_path
+# model = args.model_path
 
 llmlab_root_dir = os.getenv("LLM_LAB_ROOT_PATH")
 
 parameters = args.parameters
 parameters = json.loads(parameters)
 
-print("Starting VLLM Server", file=sys.stderr)
+print("Starting OpenAI API Proxy Server", file=sys.stderr)
 
-# Quantization is not yet supported but once it is, we need to add the following to index:
-# "quantization": {
-#     "title": "Quantization",
-#     "type": "string",
-#     "enum": [
-#             "aqlm",
-#             "awq",
-#             "deepspeedfp",
-#             "fp8",
-#             "fbgemm_fp8",
-#             "marlin",
-#             "gptq_marlin_24",
-#             "gptq_marlin",
-#             "awq_marlin",
-#             "gptq",
-#             "squeezellm",
-#             "compressed-tensors",
-#             "bitsandbytes",
-#             "None"
-#     ]
-# }
-
-# Now go through the parameters object and remove the key that is equal to "inferenceEngine":
-if "inferenceEngine" in parameters:
-    del parameters["inferenceEngine"]
-
-if "max-model-len" in parameters:
-    if parameters["max-model-len"] == "":
-        del parameters["max-model-len"]
-
-if "inferenceEngineFriendlyName" in parameters:
-    del parameters["inferenceEngineFriendlyName"]
-
-if "num_gpus" in parameters:
-    del parameters["num_gpus"]
-
-# The command to run a VLLM server is:
-# python -m vllm.entrypoints.openai.api_server --model facebook/opt-125m
-# but we can also run it through FastChat's VLLM integration:
-# https://github.com/lm-sys/FastChat/blob/main/docs/vllm_integration.md
 
 # Get plugin directory
 real_plugin_dir = os.path.realpath(os.path.dirname(__file__))
@@ -72,16 +32,11 @@ real_plugin_dir = os.path.realpath(os.path.dirname(__file__))
 # Get Python executable (from venv if available)
 python_executable = get_python_executable(real_plugin_dir)
 
-popen_args = [python_executable, "-m", "fastchat.serve.vllm_worker", "--model-path", model]
-model_dtype = parameters.get("model_dtype")
-# Set model dtype if provided
-if model_dtype is not None and model_dtype != "":
-    popen_args.extend(["--model_dtype", model_dtype])
+popen_args = [python_executable, "-m", "fastchat.serve.openai_api_proxy_worker"]
+
 
 # Add all parameters to the command
 for key, value in parameters.items():
-    if key == "model_dtype":
-        continue
     popen_args.extend([f"--{key}", str(value)])
 
 print(popen_args)
@@ -96,5 +51,5 @@ with open(f"{llmlab_root_dir}/worker.pid", "w") as f:
 for line in iter(proc.stderr.readline, b""):
     print(line, file=sys.stderr)
 
-print("VLLM Server exited", file=sys.stderr)
+print("OpenAI API Proxy Server exited", file=sys.stderr)
 sys.exit(1)  # 99 is our code for CUDA OOM
