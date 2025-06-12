d95f3353427289e353a6aa43cca6228116656210
deep1401 <gandhi0869@gmail.com>
2025-06-12 11:15:44 -0700

    Upgrade versions of tlab-inf and plugins

--- transformerlab/plugins/fastchat_server/setup.sh
+++ transformerlab/plugins/fastchat_server/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Everything should be installed by default
-uv pip install "transformerlab-inference>=0.2.38"
+uv pip install "transformerlab-inference>=0.2.39"
 
 if ! command -v rocminfo &> /dev/null; then
     uv pip install bitsandbytes
