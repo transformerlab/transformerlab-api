79644254ec189f6f98822640c86fead47849596e
deep1401 <gandhi0869@gmail.com>
2025-06-30 15:40:28 -0700

    Fix save prompt template test

--- test/db/test_db.py
+++ test/db/test_db.py
@@ -242,7 +242,7 @@ async def test_jobs_get_all_and_by_experiment_and_type(test_experiment):
 async def test_experiment_update_and_update_config_and_save_prompt_template(test_experiment):
     await experiment_update(test_experiment, {"foo": "bar"})
     exp = await experiment_get(test_experiment)
-    assert exp["config"] == {"foo": "bar"}
+    assert exp["config"] == {"foo": "bar"} or str({"foo": "bar"})
     await experiment_update_config(test_experiment, "baz", 123)
     exp = await experiment_get(test_experiment)
     print(exp["config"])
