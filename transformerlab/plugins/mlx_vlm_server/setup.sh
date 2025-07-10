dd3e3cf25fd4b0d56e149d3077008b0c82434f70
Tony Salomone <dadmobile@gmail.com>
2025-07-10 13:20:19 -0400

    Update MLX to v0.26

--- transformerlab/plugins/mlx_vlm_server/setup.sh
+++ transformerlab/plugins/mlx_vlm_server/setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-uv pip install mlx==0.23.2 --upgrade
-uv pip install "mlx-lm==0.22.1" --upgrade
+uv pip install mlx==0.26.3 --upgrade
+uv pip install "mlx-lm==0.26.0" --upgrade
 # pip install numpy transformers torch huggingface_hub Pillow
