a1301b2fb58a67ff5b6a4ac252e026a8435ff7d8
mina-parham <mina.parham766@gmail.com>
2025-07-23 14:51:03 -0400

    Refactor

--- api.py
+++ api.py
@@ -82,8 +82,8 @@ os.environ["LLM_LAB_ROOT_PATH"] = dirs.ROOT_DIR
 os.environ["_TFL_WORKSPACE_DIR"] = dirs.WORKSPACE_DIR
 os.environ["_TFL_SOURCE_CODE_DIR"] = dirs.TFL_SOURCE_CODE_DIR
 # The temporary image directory for OpenAI Proxy
-temp_image_dir = os.path.join(dirs.WORKSPACE_DIR, "temp", "openai_proxy_image")
-os.environ["OPENAI_PROXY_IMAGE_DIR"] = str(temp_image_dir)
+temp_image_dir = os.path.join(dirs.WORKSPACE_DIR, "temp", "images")
+os.environ["TEMP_IMAGE_DIR"] = str(temp_image_dir)
 
 from transformerlab.routers.job_sdk import get_xmlrpc_router, get_trainer_xmlrpc_router  # noqa: E402
 
