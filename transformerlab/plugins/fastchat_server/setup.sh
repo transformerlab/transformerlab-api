0871a1f37392ef6d890c273c886a986c8b131086
deep1401 <gandhi0869@gmail.com>
2025-08-06 10:30:38 -0400

    fix requirements

--- transformerlab/plugins/fastchat_server/setup.sh
+++ transformerlab/plugins/fastchat_server/setup.sh
@@ -11,7 +11,7 @@ if command -v nvidia-smi &> /dev/null; then
         uv pip install torch==2.8.0 torchvision torchaudio --index-url https://download.pytorch.org/whl/test/cu128
         uv pip install git+https://github.com/triton-lang/triton.git@main#subdirectory=python/triton_kernels
         uv pip install "triton==3.4.0"
-        uv pip install -r "kernels>=0.9.0" "peft>=0.17.0" "trl>=0.21.0" "trackio" "transformers>=4.55.0"
+        uv pip install "kernels>=0.9.0" "peft>=0.17.0" "trl>=0.21.0" "trackio" "transformers>=4.55.0"
     fi
 fi
 
