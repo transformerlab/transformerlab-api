7c78e2d32af7d689ccb8e2f79ef19f5aa7698907
deep1401 <gandhi0869@gmail.com>
2025-08-07 13:45:11 -0400

    Add support for lower GPUs MXFP4

--- transformerlab/plugins/fastchat_server/setup.sh
+++ transformerlab/plugins/fastchat_server/setup.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
-# Everything should be installed by default
+
+if ! command -v rocminfo &> /dev/null; then
+    uv pip install bitsandbytes
+fi
+
+# For GPT-OSS
 if command -v nvidia-smi &> /dev/null; then
     echo "GPU Compute Capability: $(nvidia-smi --query-gpu=compute_cap --format=csv,noheader,nounits)"
     
@@ -8,13 +13,17 @@ if command -v nvidia-smi &> /dev/null; then
     # Use awk to compare floating point numbers
     if awk "BEGIN {exit !($min_compute_cap > 9.0)}"; then
         echo "All GPUs have compute capability > 9.0"
+        uv pip install openai_harmony
         uv pip install torch==2.8.0 torchvision torchaudio --index-url https://download.pytorch.org/whl/test/cu128
         uv pip install git+https://github.com/triton-lang/triton.git@main#subdirectory=python/triton_kernels
         uv pip install "triton==3.4.0"
         uv pip install "kernels>=0.9.0" "peft>=0.17.0" "trl>=0.21.0" "trackio" "transformers>=4.55.0"
+    else
+        uv pip install openai_harmony
+        uv pip install -e /home/transformerlab/workspace/transformerlab-inference/
+        uv pip install git+https://github.com/triton-lang/triton.git@main#subdirectory=python/triton_kernels
+        uv pip install "kernels>=0.9.0" "peft>=0.17.0" "trl>=0.21.0" "trackio"
+        uv pip install git+https://github.com/huggingface/transformers.git 
+        uv pip install "triton==3.4.0"
     fi
-fi
-
-if ! command -v rocminfo &> /dev/null; then
-    uv pip install bitsandbytes
 fi
