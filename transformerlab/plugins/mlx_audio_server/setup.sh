e11b41e507e7780db524090e5286772ed5277b93
mina-parham <mina.parham766@gmail.com>
2025-08-27 10:48:11 -0400

    remove pinned mlx version and spacy

--- transformerlab/plugins/mlx_audio_server/setup.sh
+++ transformerlab/plugins/mlx_audio_server/setup.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 #uv pip install mlx==0.24.0 # Using this version to work around bug: https://github.com/Blaizzy/mlx-audio/issues/207
-uv pip install "mlx-lm==0.24.0" 
 uv pip install "mlx-audio"
-uv pip install spacy
 python -m ensurepip --upgrade
