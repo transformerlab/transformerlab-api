bd7029690546e936e78a7bb97be861aa8d09cfb9
mina-parham <mina.parham766@gmail.com>
2025-09-22 12:27:53 -0400

    ruff

--- transformerlab/routers/model.py
+++ transformerlab/routers/model.py
@@ -25,7 +25,7 @@ from transformerlab.models import basemodel
 from transformerlab.models import huggingfacemodel
 from transformerlab.models import filesystemmodel
 from transformerlab.services.job_service import job_update_status
-from lab import WORKSPACE_DIR, dirs as lab_dirs
+from lab import WORKSPACE_DIR
 from transformerlab.shared import dirs
 
 from werkzeug.utils import secure_filename
