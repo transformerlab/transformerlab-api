d6d13dcc37b6cab561001cefee5b8a9645112f5e
Tony Salomone <dadmobile@gmail.com>
2025-09-23 20:22:01 -0400

    Add function to get experiment from SDK by database ID

--- transformerlab/services/experiment_service.py
+++ transformerlab/services/experiment_service.py
@@ -1,1 +1,18 @@
+from transformerlab.db.db import experiment_get
 from lab import Experiment
+
+
+async def get_experiment_by_id(experimentId: int):
+    """
+    Helper function to get SDK Experiment from an ID.
+    """
+    # first get the experiment name:
+    data = await experiment_get(experimentId)
+
+    # if the experiment does not exist, return none
+    if data is None:
+        return None
+
+    experiment_name = data["name"]
+    experiment = Experiment.get(experiment_name)
+    return experiment
