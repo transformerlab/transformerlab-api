7e616fecffa9ac31fbfbdae8dab49515e486f22f
paramthakkar123 <paramthakkar864@gmail.com>
2025-10-30 17:45:17 +0530

    ruff format

--- transformerlab/db/db.py
+++ transformerlab/db/db.py
@@ -13,7 +13,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from typing import AsyncGenerator
 
 from transformerlab.shared.models import models
-from transformerlab.shared.models.models import Config, Plugin
+from transformerlab.shared.models.models import Config
 from transformerlab.db.utils import sqlalchemy_to_dict, sqlalchemy_list_to_dict
 
 from transformerlab.db.session import async_session
