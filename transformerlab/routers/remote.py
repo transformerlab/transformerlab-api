e0246cf96857c7905224f165911900fcd61b0ccc
Mina Parham <mina.parham766@gmail.com>
2025-11-03 17:27:21 -0500

    Restore stream_logs from main

--- transformerlab/routers/remote.py
+++ transformerlab/routers/remote.py
@@ -371,6 +371,7 @@ async def upload_directory(
     Upload a directory to the remote Lattice orchestrator for later use in cluster launches.
     Files are stored locally first, then sent to orchestrator.
     """
+    from lab.dirs import get_workspace_dir
 
     # Validate environment variables
     result = validate_gpu_orchestrator_env_vars()
