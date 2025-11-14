ea122b5043443da4c556ed18cb8833ec6d21809f
deep1401 <gandhi0869@gmail.com>
2025-11-14 09:58:18 -0500

    fix setup

--- transformerlab/plugins/dllm_server/setup.sh
+++ transformerlab/plugins/dllm_server/setup.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 # Install dllm from GitHub
-# uv pip install git+https://github.com/ZHZisZZ/dllm.git --upgrade
 git clone https://github.com/ZHZisZZ/dllm
 cd dllm 
 uv pip install -e .
