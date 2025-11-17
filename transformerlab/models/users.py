4481d2d3a48988ca18d0cbd63a21c7aef1b88e93
ali asaria <aliasaria@users.noreply.github.com>
2025-11-17 07:05:26 -0500

    add more logging

--- transformerlab/models/users.py
+++ transformerlab/models/users.py
@@ -39,6 +39,12 @@ class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
     async def on_after_register(self, user: User, request: Optional[Request] = None):
         print(f"User {user.id} has registered.")
 
+    async def on_after_forgot_password(self, user: User, token: str, request: Request | None = None):
+        print(f"User {user.id} has forgot their password. Reset token: {token}")
+
+    async def on_after_request_verify(self, user: User, token: str, request: Request | None = None):
+        print(f"Verification requested for user {user.id}. Verification token: {token}")
+
 
 async def get_user_db(session: AsyncSession = Depends(get_async_session)):
     yield SQLAlchemyUserDatabase(session, User)
